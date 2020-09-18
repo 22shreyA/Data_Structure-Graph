8422a5df028a6a5e082b0e5fc30ad43fb2416fc0
22shreyA <55088999+22shreyA@users.noreply.github.com>
2020-09-19 01:05:27 +0530

    Detecting Cycle in a directed graph

---
+++ sh_find cycle graph.cpp
@@ -0,0 +1,89 @@
+#include<bits/stdc++.h> 
+  
+using namespace std; 
+  
+class Graph 
+{ 
+    int V;    
+    list<int> *adj;    
+    bool isCyclicUtil(int v, bool visited[], bool *rs);  
+public: 
+    Graph(int V);   // Constructor 
+    void addEdge(int v, int w);   
+    bool isCyclic();     
+}; 
+  
+Graph::Graph(int V) 
+{ 
+    this->V = V; 
+    adj = new list<int>[V]; 
+} 
+  
+void Graph::addEdge(int v, int w) 
+{ 
+    adj[v].push_back(w) ;
+} 
+  
+bool Graph::isCyclicUtil(int v, bool visited[], bool *recStack) 
+{ 
+    if(visited[v] == false) 
+    { 
+    
+        visited[v] = true; 
+        recStack[v] = true; 
+  
+        
+        list<int>::iterator i; 
+        for(i = adj[v].begin(); i != adj[v].end(); ++i) 
+        { 
+            if ( !visited[*i] && isCyclicUtil(*i, visited, recStack) ) 
+                return true; 
+            else if (recStack[*i]) 
+                return true; 
+        } 
+  
+    } 
+    recStack[v] = false;  // remove the vertex from recursion stack 
+    return false; 
+} 
+  
+bool Graph::isCyclic() 
+{ 
+    
+    bool *visited = new bool[V]; 
+    bool *recStack = new bool[V]; 
+    for(int i = 0; i < V; i++) 
+    { 
+        visited[i] = false; 
+        recStack[i] = false; 
+    } 
+  
+    
+     
+    for(int i = 0; i < V; i++) 
+        if (isCyclicUtil(i, visited, recStack)) 
+            return true; 
+  
+    return false; 
+} 
+  
+int main() 
+{ 
+    int vertices,e;
+    cin>>vertices>>e;
+    Graph g(vertices); 
+    
+    int x,y;
+    while(e--){
+    cin>>x>>y;
+     g.addEdge(x, y);
+    }
+    
+     
+  
+    if(g.isCyclic()) 
+        cout << "Graph contains cycle"; 
+    else
+        cout << "Graph doesn't contain cycle"; 
+    return 0; 
+} 
